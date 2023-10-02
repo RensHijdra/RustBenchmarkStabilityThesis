44c51a120fae2f3de1817dc434e1966bf5d70943
Rens Hijdra <rhijdra2@hotmail.com>
2023-10-02 16:56:49 +0200

    add additional baseline benches

--- projects/baseline/benches/nop.rs
+++ projects/baseline/benches/nop.rs
@@ -3,14 +3,11 @@ use std::arch::asm;
 use criterion::{criterion_group, criterion_main, Criterion};
 use criterion_energy::msr::measurement::Energy;
 
-fn simple(c: &mut Criterion::<Energy>) {
-    {
-        let mut group = c.benchmark_group("nops");
-        group.sampling_mode(criterion::SamplingMode::Flat);
-        
-        group.bench_function("nops", |bencher| {
+macro_rules! nop {
+    ($group:expr, $number:literal) => {
+        $group.bench_function("$number", |bencher| {
             bencher.iter(|| {
-                for _ in 0..100 {
+                for _ in 0..$number {
                     unsafe {
                         asm!{
                             "nop"
@@ -19,8 +16,23 @@ fn simple(c: &mut Criterion::<Energy>) {
                 }
             });
         });
+    };
+}
+
+fn simple(c: &mut Criterion::<Energy>) {
+    {
+        let mut group = c.benchmark_group("nops");
+        group.sampling_mode(criterion::SamplingMode::Flat);
+        
+        nop!(group, 100);
+        nop!(group, 1000);
+        nop!(group, 10000);
+        nop!(group, 100000);
+        nop!(group, 1000000);
     }
 }
+
+
 
 criterion_group!(name=benches; config = Criterion::default().with_measurement(Energy); targets =  simple);
 criterion_main!(benches);
